fc01822944bf69885200d4bd48df0fec9376fe3f
AQUINO SANCHEZ Jorge <jorge.aquinosanchez@ext.europarl.europa.eu>
2025-10-22 15:41:21 +0200

    update meetings oct

--- init.py
+++ init.py
@@ -8,8 +8,8 @@ if __name__ == "__main__":
     config = read_configuration_file("config.yaml")
 
     """ Get meetings resources for each year configurated in the config.yaml file and all decisions a single EP Meeting ID. """
-    #meetings_year = api_meetings_ep(config)
-    #meetings_year.get_meeting_resources()
+    meetings_year = api_meetings_ep(config)
+    meetings_year.get_meeting_resources()
 
     """ Read all decision files and generate a dataset """
     statistic_votes = meps_decision(config)
